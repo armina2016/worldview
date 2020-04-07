ae7a329804e06ba73e84d1995536c331b0a59942
Benjamin King <benking4@gmail.com>
2020-04-07 17:53:54 -0400

    wip minimap labels

--- web/js/map/mapOverview.js
+++ web/js/map/mapOverview.js
@@ -1,12 +1,23 @@
 import OverviewMap from 'ol/control/OverviewMap';
 import OlView from 'ol/View';
 import * as olProj from 'ol/proj';
-import { getLine } from './util';
+import { getLine, getTextVectorLayer } from './util';
+import util from '../util/util';
 
+function getDateLabelArray (date) {
+  return [-180, 180].map((x, i) => {
+    const dateArray = i === 0 ? [util.toISOStringDate(util.dateAdd(date, 'day', 1)), util.toISOStringDate(date)] : [util.toISOStringDate(date), util.toISOStringDate(util.dateAdd(date, 'day', -1))];
+    return { coordinate: [x, 0], dateArray };
+  });
+}
 export function getOverviewControl(def, date, projCRS, createLayer) {
   const minimapLineLayer1 = getLine([[-180, 300], [-180, -300]], 4, 'red', 0, [2, 5]);
   const minimapLineLayer2 = getLine([[180, 300], [180, -300]], 4, 'red', 0, [2, 5]);
-  const layerDefault = createLayer(def, {
+  const dateLayerArray = getDateLabelArray(date);
+
+  const dateLayer = getTextVectorLayer(dateLayerArray);
+  console.log(dateLayer);
+  const backgroundLayer = createLayer(def, {
     matrixIds: [2, 3, 4, 5, 6, 7, 8],
     resolutions: [0.140625, 0.0703125, 0.03515625, 0.017578125, 0.0087890625, 0.00439453125, 0.002197265625],
     date,
@@ -14,7 +25,7 @@ export function getOverviewControl(def, date, projCRS, createLayer) {
     extent: [-180, -90, 180, 90],
     isOverview: true,
   });
-  layerDefault.setVisible(true);
+  backgroundLayer.setVisible(true);
   const map = new OverviewMap({
   // see in overviewmap-custom.html to see the custom CSS used
     className: 'ol-overviewmap ol-custom-overviewmap',
@@ -22,8 +33,8 @@ export function getOverviewControl(def, date, projCRS, createLayer) {
     layers: [
       minimapLineLayer1,
       minimapLineLayer2,
-      layerDefault,
-
+      backgroundLayer,
+      dateLayer,
     ],
     view: new OlView({
       projection: olProj.get(projCRS),
--- web/js/map/util.js
+++ web/js/map/util.js
@@ -1,9 +1,12 @@
 import OlStyle from 'ol/style/Style';
 import OlStroke from 'ol/style/Stroke';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
+import OlVectorLayer from 'ol/layer/Vector';
+import OlVectorSource from 'ol/source/Vector';
 import OlFeature from 'ol/Feature';
-import LineString from 'ol/geom/LineString';
+import OlLineString from 'ol/geom/LineString';
+import OlPoint from 'ol/geom/Point';
+import OlFill from 'ol/style/Fill';
+import OlText from 'ol/style/Text';
 
 const ZOOM_DURATION = 250;
 
@@ -61,10 +64,10 @@ function getLineStyle(color, width, lineDash) {
   });
 }
 export function getLine(coordinateArray, width, color, opacity, lineDash) {
-  return new VectorLayer({
-    source: new VectorSource({
+  return new OlVectorLayer({
+    source: new OlVectorSource({
       features: [new OlFeature({
-        geometry: new LineString(coordinateArray),
+        geometry: new OlLineString(coordinateArray),
       })],
     }),
     zIndex: Infinity,
@@ -73,3 +76,54 @@ export function getLine(coordinateArray, width, color, opacity, lineDash) {
     style: [getLineStyle('black', width, lineDash), getLineStyle(color, width / 2, lineDash)],
   });
 }
+function getDatelineTextStyle(feature) {
+  const dateArray = feature.get('dateArray');
+  const font = '13px Open Sans Bold Arial, Unicode MS Bold';
+
+  return dateArray.map((dateStr, index) => {
+    // const util.getTextWidth
+    const isLeft = index === 0;
+    return new OlStyle({
+      text: new OlText({
+        textAlign: isLeft ? 'left' : 'right',
+        font,
+        text: dateStr,
+        fill: new OlFill({ color: '#fff' }),
+        stroke: new OlStroke({ color: '#000', width: '1px' }),
+        offsetX: isLeft ? -10 : 10,
+      }),
+    });
+  });
+}
+export function getTextVectorLayer(labelArray) {
+  return new OlVectorLayer({
+    source: new OlVectorSource({
+      features: labelArray.map(({ dateArray, coordinate }) => new OlFeature({
+        geometry: new OlPoint({ coordinates: [coordinate] }),
+        dateArray,
+        coordinate,
+      })),
+    }),
+    zIndex: Infinity,
+    wrapX: false,
+    style: getDatelineTextStyle,
+  });
+}
+
+/**
+ *
+ * @param {String} className String of overlay element classNames
+ * @param {Array} coordinate Point on map where element should be appended
+ * @param {String} text Desired text
+ */
+// export function getTextOverlay(className, coordinate, text) {
+//   const lineCase = document.createElement('div');
+//   lineCase.className = className;
+//   lineCase.appendChild(document.createTextNode(text));
+//   const overlay = new OlOverlay({
+//     element: lineCase,
+//     stopEvent: false,
+//   });
+//   overlay.setPosition(coordinate);
+//   return overlay;
+// }
